48d47a5e1c40aee134732b7071fc26dc54f647b5
tonnidiaz <therealhackingknight@gmail.com>
2023-10-27 18:36:25 +0200

    test runner

---
+++ index.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import pino from 'pino';
+
+const server = fastify({
+    logger: pino({
+        level: "info"
+    })
+})
+
+server.get("/", (req, res)=>{
+    return "Hello"
+})
+server.get('/ping', async (request, reply) => {
+  return 'pong dawg\n'
+})
+
+server.listen({ port: 8000 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
