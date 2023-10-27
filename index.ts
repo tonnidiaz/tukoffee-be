e8841ebb5e90f7495b5d10efc71d90f3d1db2245
tonnidiaz <therealhackingknight@gmail.com>
2023-10-27 18:43:48 +0200

    host to 0.0.0.0

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ server.get('/ping', async (request, reply) => {
   return 'pong dawg\n'
 })
 
-server.listen({ port: 8000 }, (err, address) => {
+server.listen({ port: 8000, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
